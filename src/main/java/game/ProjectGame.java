fb15e0e870c1d63fa06fdcf4b28c7c360ca093fb
Tyler <tlr8cn@virginia.edu>
2017-04-10 02:33:25 -0400

    Some collisions and an unfinished level

--- src/main/java/game/ProjectGame.java
+++ src/main/java/game/ProjectGame.java
@@ -32,13 +32,44 @@ public class ProjectGame extends Game {
   //  Platformer platform1 = new Platformer("Rectangele", "platform.png");
     boolean start = false;
     SoundManagerClass music = new SoundManagerClass();
-    Sprite marioBackground = new Sprite("Background", "dungeon_concept.png");
+    Sprite background = new Sprite("Background", "background.png");
     ArrayList<Platformer> collisionArray = new ArrayList<Platformer>();
     Bullet bullet;
     private AnimatedSprite player;
     private Enemy enemy;
 
     ArrayList<Double> listArray = new ArrayList<Double>();
+
+    //map stuff
+    Sprite tile1;
+    Sprite tile2;
+    Sprite tile3;
+    Platformer tile4;
+    Sprite tile5;
+    Sprite tile6;
+    Sprite tile7;
+    Platformer tile8;
+    Sprite tile9;
+    Sprite tile10;
+    Sprite tile11;
+    Sprite tile12;
+    Sprite tile13;
+    Sprite tile14;
+    Sprite tile15;
+    Platformer tile16;
+
+    Sprite ctile1;
+    Sprite ctile2;
+    Sprite ctile3;
+    Sprite ctile4;
+
+    Platformer back1;
+    Platformer back2;
+    Platformer back3;
+    Platformer back4;
+
+
+
 
 
     int offset = 1;
@@ -66,9 +97,10 @@ public class ProjectGame extends Game {
         this.addEventListener(myQuestManager, collidedEvent.getEventType());
 
 
-       // collisionArray.add(platform);
 
 
+        background.setScaleX(5);
+        background.setScaleY(5);
 
 
 
@@ -89,7 +121,7 @@ public class ProjectGame extends Game {
       //  music.playMusic("resources/bowsersound.mp3");
 
 
-        player.setPositionX(750);
+        player.setPositionX(400);
         player.setPositionY(450);
 
 
@@ -98,10 +130,10 @@ public class ProjectGame extends Game {
         enemy.setDelay(100);
         enemy.setPositionX(570);
         enemy.setPositionY(200);
-        enemy.addRoute(150,0,1);//create square route
+        enemy.addRoute(125,0,1);//create square route
         enemy.addRoute(5,0,0);
         enemy.addRoute(0,-300,8);
-        enemy.addRoute(-150,0,2);
+        enemy.addRoute(-125,0,2);
         enemy.addRoute(0,300,4);
 
 
@@ -110,17 +142,122 @@ public class ProjectGame extends Game {
 
         coin.setPositionY(250);
         coin.setPositionX(660);
-        TweenTransitions playerIntro = new TweenTransitions("linearTransition");
-        Tween playerTween = new Tween(player, playerIntro);
-        playerTween.animate(TweenableParams.Y, 1000, 650, 2);
+//        TweenTransitions playerIntro = new TweenTransitions("linearTransition");
+//        Tween playerTween = new Tween(player, playerIntro);
+//        playerTween.animate(TweenableParams.Y, 1000, 650, 2);
 
 
         TweenTransitions coinCatch = new TweenTransitions("easeInOut");
 
 
-        TweenJuggler.getInstance().add(playerTween);
+  //      TweenJuggler.getInstance().add(playerTween);
 
 
+        tile1 = new Sprite("tile1", "tile.png");
+        tile1.setPositionX(256);
+        tile1.setPositionY(300);
+
+        tile2 = new Sprite("tile2", "tile.png");
+        tile2.setPositionX(tile1.getPositionX());
+        tile2.setPositionY(tile1.getPositionY() + tile1.getUnscaledHeight());
+
+        tile3 = new Sprite("tile3", "tile.png");
+        tile3.setPositionX(tile1.getPositionX());
+        tile3.setPositionY(tile2.getPositionY() + tile2.getUnscaledHeight());
+
+        tile4 = new Platformer("tile4", "dark_brick.png");
+        tile4.setPositionX(tile1.getPositionX());
+        tile4.setPositionY(tile3.getPositionY() + tile3.getUnscaledHeight());
+
+        tile5 = new Sprite("tile5", "tile.png");
+        tile5.setPositionX(tile1.getPositionX() + tile1.getUnscaledWidth());
+        tile5.setPositionY(tile1.getPositionY());
+
+        tile6 = new Sprite("tile6", "tile.png");
+        tile6.setPositionX(tile2.getPositionX() + tile2.getUnscaledWidth());
+        tile6.setPositionY(tile2.getPositionY());
+
+
+        tile7 = new Sprite("tile7", "tile.png");
+        tile7.setPositionX(tile3.getPositionX() + tile3.getUnscaledWidth());
+        tile7.setPositionY(tile3.getPositionY());
+
+        tile8 = new Platformer("tile8", "dark_brick.png");
+        tile8.setPositionX(tile4.getPositionX() + tile4.getUnscaledWidth());
+        tile8.setPositionY(tile4.getPositionY());
+
+        tile9 = new Sprite("tile9", "tile.png");
+        tile9.setPositionX(tile5.getPositionX() + tile5.getUnscaledWidth());
+        tile9.setPositionY(tile5.getPositionY());
+
+        tile10 = new Sprite("tile10", "tile.png");
+        tile10.setPositionX(tile6.getPositionX() + tile6.getUnscaledWidth());
+        tile10.setPositionY(tile6.getPositionY());
+
+        tile11 = new Sprite("tile11", "tile.png");
+        tile11.setPositionX(tile7.getPositionX() + tile7.getUnscaledWidth());
+        tile11.setPositionY(tile7.getPositionY());
+
+        tile12 = new Sprite("tile12", "tile_dark.png");
+        tile12.setPositionX(tile8.getPositionX() + tile8.getUnscaledWidth());
+        tile12.setPositionY(tile8.getPositionY());
+
+        tile13 = new Sprite("tile13", "tile.png");
+        tile13.setPositionX(tile9.getPositionX() + tile9.getUnscaledWidth());
+        tile13.setPositionY(tile9.getPositionY());
+
+        tile14 = new Sprite("tile14", "tile.png");
+        tile14.setPositionX(tile10.getPositionX() + tile10.getUnscaledWidth());
+        tile14.setPositionY(tile10.getPositionY());
+
+        tile15 = new Sprite("tile15", "tile.png");
+        tile15.setPositionX(tile11.getPositionX() + tile11.getUnscaledWidth());
+        tile15.setPositionY(tile11.getPositionY());
+
+        tile16 = new Platformer("tile16", "dark_brick.png");
+        tile16.setPositionX(tile12.getPositionX() + tile12.getUnscaledWidth());
+        tile16.setPositionY(tile12.getPositionY());
+
+        ctile1 = new Sprite("ctile1", "tile_with_column.png");
+        ctile1.setPositionX(tile1.getPositionX());
+        ctile1.setPositionY(tile1.getPositionY() - tile1.getUnscaledHeight());
+
+
+        ctile2 = new Sprite("ctile2", "tile_with_column.png");
+        ctile2.setPositionX(tile5.getPositionX());
+        ctile2.setPositionY(tile5.getPositionY() - tile5.getUnscaledHeight());
+
+        ctile3 = new Sprite("ctile3", "tile_with_column.png");
+        ctile3.setPositionX(tile9.getPositionX());
+        ctile3.setPositionY(tile9.getPositionY() - tile9.getUnscaledHeight());
+
+        ctile4 = new Sprite("ctile4", "tile_with_column.png");
+        ctile4.setPositionX(tile13.getPositionX());
+        ctile4.setPositionY(tile13.getPositionY() - tile13.getUnscaledHeight());
+
+        back1 = new Platformer("back", "back_wall_column_left.png");
+        back1.setPositionX(ctile1.getPositionX());
+        back1.setPositionY(ctile1.getPositionY() - ctile1.getUnscaledHeight());
+
+        back2 = new Platformer("back", "back_wall_column_right.png");
+        back2.setPositionX(ctile2.getPositionX());
+        back2.setPositionY(ctile2.getPositionY() - ctile2.getUnscaledHeight());
+
+        back3 = new Platformer("back", "back_wall_column_left.png");
+        back3.setPositionX(ctile3.getPositionX());
+        back3.setPositionY(ctile3.getPositionY() - ctile3.getUnscaledHeight());
+
+        back4 = new Platformer("back", "back_wall_column_right.png");
+        back4.setPositionX(ctile4.getPositionX());
+        back4.setPositionY(ctile4.getPositionY() - ctile4.getUnscaledHeight());
+
+        collisionArray.add(tile16);
+        collisionArray.add(tile8);
+        collisionArray.add(tile4);
+        collisionArray.add(back1);
+        collisionArray.add(back2);
+        collisionArray.add(back3);
+        collisionArray.add(back4);
     }
 
 
@@ -360,10 +497,35 @@ public class ProjectGame extends Game {
         super.draw(g);
 
 
-        if (marioBackground != null) {
-            marioBackground.draw(g);
+        if (background != null) {
+            background.draw(g);
 
         }
+
+        tile1.draw(g);
+        tile2.draw(g);
+        tile3.draw(g);
+        tile4.draw(g);
+        tile5.draw(g);
+        tile6.draw(g);
+        tile7.draw(g);
+        tile8.draw(g);
+        tile9.draw(g);
+        tile10.draw(g);
+        tile11.draw(g);
+        tile12.draw(g);
+        tile13.draw(g);
+        tile14.draw(g);
+        tile15.draw(g);
+        tile16.draw(g);
+        ctile1.draw(g);
+        ctile2.draw(g);
+        ctile3.draw(g);
+        ctile4.draw(g);
+        back1.draw(g);
+        back2.draw(g);
+        back3.draw(g);
+        back4.draw(g);
 
         if(bullet != null){
             bullet.draw(g);
