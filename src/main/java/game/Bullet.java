d4ce23ff510a0a185509354c4f1b9f15ffe048bc
Brigham Bangerter <bb3uc@virginia.edu>
2017-04-28 15:55:52 -0400

    updated shooting

--- src/main/java/game/Bullet.java
+++ src/main/java/game/Bullet.java
@@ -18,6 +18,8 @@ public class Bullet extends Sprite implements IEventListener {
     double endValY;
     double startValX;
     double startValY;
+    int velocityX;
+    int velocityY;
 
     double shotTimer;
     double shotCap;
@@ -29,9 +31,18 @@ public class Bullet extends Sprite implements IEventListener {
         super(id);
     }
 
-    public Bullet(String id, String imageFileName, double shot_cap) {
+    public Bullet(String id, String imageFileName, double shot_cap, double startX, double startY, double endX, double endY) {
 
         super(id, imageFileName);
+
+//        setStart(startX, startY);
+//        setEnd(endX,endY);
+
+        double angle = Math.atan2(endX - startX, endY-startY);
+        velocityX = (int)Math.cos(angle)*1;
+        velocityY = (int)Math.sin(angle)*1;
+
+
 
         shotTimer = 0.0;
         shotCap = shot_cap;
@@ -40,6 +51,31 @@ public class Bullet extends Sprite implements IEventListener {
 
         setPivotY(getUnscaledHeight()/2);
         setPivotX(getUnscaledWidth()/2);
+
+        //1)take in mouse location
+        //2)take in start position
+        //3)
+    }
+
+    public Bullet(String id, String imageFileName, double shot_cap) {
+
+        super(id, imageFileName);
+
+
+
+
+
+        shotTimer = 0.0;
+        shotCap = shot_cap;
+
+        startTime = (double) System.currentTimeMillis() / 1000;
+
+        setPivotY(getUnscaledHeight()/2);
+        setPivotX(getUnscaledWidth()/2);
+
+        //1)take in mouse location
+        //2)take in start position
+        //3)
     }
 
     @Override
@@ -68,9 +104,11 @@ public class Bullet extends Sprite implements IEventListener {
         super.update(pressedKeys);
 
         shotTimer = ( (double) System.currentTimeMillis() / 1000) - startTime;
-
-
+        setPositionX(getPositionX()+velocityX);
+        setPositionY(getPositionY()+velocityY);
         setRotation(getRotation() + 1);
+
+        System.out.println(getPositionX());
     }
 
     public double getShotTimer() { return shotTimer; }
--- src/main/java/game/ProjectGame.java
+++ src/main/java/game/ProjectGame.java
@@ -596,11 +596,11 @@ public class ProjectGame extends Game {
                 g.setColor(Color.RED);
                 g.drawString("BULLET HELL", 450, 120);
                 g.setColor(Color.WHITE);
-                g.drawString("Play", getUnscaledWidth() / 2 + 550, 210);
+                g.drawString("Play", getUnscaledWidth() / 2 + 580, 210);
                 g.drawRect(getUnscaledWidth() / 2 + 530, 170, 180, 50);
 //                g.drawString("Help",getUnscaledWidth()/2+550,310);
 //                g.drawRect(getUnscaledWidth()/2+ 530,270,150,50);
-                g.drawString("Quit", getUnscaledWidth() / 2 + 550, 410);
+                g.drawString("Quit", getUnscaledWidth() / 2 + 580, 410);
                 g.drawRect(getUnscaledWidth() / 2 + 530, 370, 150, 50);
             }
         } else if (state == STATE.GAME) {
@@ -723,7 +723,7 @@ public class ProjectGame extends Game {
             double mouseX = e.getX();
             double mouseY = e.getY();
             if (mouseX >= getUnscaledWidth() / 2 + 530 && mouseX <= getUnscaledWidth() / 2 + 680) {
-                if (mouseY >= 170 && mouseY <= 220) {
+                if (mouseY >= 200 && mouseY <= 260) {
                     state = STATE.GAME;
                 }
             }
@@ -736,22 +736,26 @@ public class ProjectGame extends Game {
             }
         } else if (state == STATE.GAME) {
             if (!player.isDead && knifeCount != 0) {
-                Bullet bul = new Bullet("bullet", "knife.png", 0.2);
+
                 double mouseX = e.getX();
                 double mouseY = e.getY();
-                double[] pressed = {mouseX, mouseY};
+                Bullet bul = new Bullet("bullet", "knife.png", 0.2, player.getPositionX()/2,player.getPositionY()/2, mouseX, mouseY);
+                addChild(bul);
 //                System.out.print(pressed[0]);
 
                 knifeCount--;
-                bul.setStart(player.getPositionX() + player.getUnscaledWidth() / 2, player.getPositionY() + player.getUnscaledHeight() / 2);
-                bul.setEnd(mouseX, mouseY);
 
 
-                TweenTransitions bulletPath = new TweenTransitions("linearTransition");
-                Tween bulletmovement = new Tween(bul, bulletPath);
-                bulletmovement.animate(TweenableParams.X, bul.startValX, bul.endValX, 0.2);
-                bulletmovement.animate(TweenableParams.Y, bul.startValY, bul.endValY, 0.2);
-                TweenJuggler.getInstance().add(bulletmovement);
+//                bul.setStart(player.getPositionX() + player.getUnscaledWidth() / 2, player.getPositionY() + player.getUnscaledHeight() / 2);
+//                bul.setEnd(mouseX+this.getUnscaledWidth(), mouseY+this.getUnscaledHeight());
+
+
+
+//                TweenTransitions bulletPath = new TweenTransitions("linearTransition");
+//                Tween bulletmovement = new Tween(bul, bulletPath);
+//                bulletmovement.animate(TweenableParams.X, bul.startValX, bul.endValX+this.getUnscaledWidth(), 0.2);
+//                bulletmovement.animate(TweenableParams.Y, bul.startValY, bul.endValY+this.getUnscaledHeight(), 0.2);
+//                TweenJuggler.getInstance().add(bulletmovement);
 
                 player.playerBullets.add(bul);
 
